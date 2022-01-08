1c9d91fd1549048706b205e0c488c7c4e96b9b17
adhirajChauhan <adhirajsingh0105@gmail.com>
2022-01-08 16:51:46 +0530

    Adding Project with camera issue

---
+++ Roll The Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - _player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _player.transform.position + _offset;
+    }
+}
---
+++ Roll The Ball/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private int _speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rb.AddForce(new Vector3(h, 0, v) * _speed * Time.deltaTime);
+    }
+}
+    
